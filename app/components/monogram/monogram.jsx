55fc45068378c2ff38b4c67bb46b3509f8e0d7ad
AhmedImtiazKhan <aimtiazk.ce41ceme@ce.ceme.edu.pk>
2025-06-09 07:01:02 +0500

    Articles page made

--- app/components/monogram/monogram.jsx
+++ app/components/monogram/monogram.jsx
@@ -1,30 +1,33 @@
-import { forwardRef } from 'react';
+import { forwardRef, useId } from 'react';
 import { classes } from '~/utils/style';
 import styles from './monogram.module.css';
 
 export const Monogram = forwardRef(({ highlight, className, ...props }, ref) => {
+  const id = useId();
+  const clipId = `${id}monogram-clip`;
+
   return (
     <svg
       aria-hidden
       className={classes(styles.monogram, className)}
-      width="160"
-      height="50"
-      viewBox="0 0 160 50"
+      width="48"
+      height="29"
+      viewBox="0 0 48 29"
       ref={ref}
       {...props}
     >
-   <text
-  x="50%"
-  y="60%"
-  dominantBaseline="middle"
-  textAnchor="middle"
-  fontSize="32"
-  fontFamily="Pacifico, cursive"
-  fill="currentColor"
->
-  Ahmed
-</text>
-
+      <defs>
+        <clipPath id={clipId}>
+          {/* SVG path for the letter A */}
+          <path d="M14.5 0L0 29h5.7l3.5-7.3h10.7l3.5 7.3h5.7L18.5 0H14.5zm-3.7 15.5L14.5 7.7 17.3 15.5h-5.9z" />
+        </clipPath>
+      </defs>
+      <rect clipPath={`url(#${clipId})`} width="100%" height="100%" />
+      {highlight && (
+        <g clipPath={`url(#${clipId})`}>
+          <rect className={styles.highlight} width="100%" height="100%" />
+        </g>
+      )}AV
     </svg>
   );
 });
